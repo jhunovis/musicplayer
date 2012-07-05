710c8a5f6840b495aff99ca3146c620877c6d085
Jan Hackel <jhunovis+github@googlemail.com>
2012-07-05 10:21:32 +0200

    Some pretty printing. Somehow the changes to the test-file were missing from the last commit.

--- android/src/jhunovis/musicplayer/MusicPlayer.java
+++ android/src/jhunovis/musicplayer/MusicPlayer.java
@@ -14,36 +14,40 @@ import android.widget.Toast;
 import java.util.*;
 import java.io.IOException;
 
-/** Allmost all of the service stub code is lifted from the API docs of class Service.
+/**
+ * Allmost all of the service stub code is lifted from the API docs of class
+ * Service.
  * 
  * @author bon
- *
+ * 
  */
-public class MusicPlayer extends android.app.Service{
-	
+public class MusicPlayer extends android.app.Service {
+
 	// Service itself is a context
 	// private Context mContext;
-	private MediaPlayer mMediaPlayer;	
+	private MediaPlayer mMediaPlayer;
 	private List<String> mPlaylist;
-	
-	
-//	public MusicPlayer(){
-		// mContext = c;
-//	}
-	
+
+	// public MusicPlayer(){
+	// mContext = c;
+	// }
+
 	public void setPlaylist(List<String> playlist) {
-		if ( playlist == null ) mPlaylist = new ArrayList<String>();
-		else mPlaylist = playlist;
+		if (playlist == null)
+			mPlaylist = new ArrayList<String>();
+		else
+			mPlaylist = playlist;
 	}
-	
+
 	public List<String> getPlaylist() {
 		return mPlaylist;
 	}
-	
-	public void play() throws IOException{
-		if (isPlaying()) return;
+
+	public void play() throws IOException {
+		if (isPlaying())
+			return;
 		String currentTrack = currentTrack();
-		if ( currentTrack != null ) {
+		if (currentTrack != null) {
 			mMediaPlayer = new MediaPlayer();
 			mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 			mMediaPlayer.setDataSource(this, Uri.parse(currentTrack));
@@ -51,82 +55,88 @@ public class MusicPlayer extends android.app.Service{
 			mMediaPlayer.start();
 		}
 	}
-	
+
 	public boolean isPlaying() {
 		return false;
 	}
-	
+
 	public String currentTrack() {
-		if ( mPlaylist.isEmpty() )
+		if (mPlaylist.isEmpty())
 			return null;
-		else return mPlaylist.get(0);
+		else
+			return mPlaylist.get(0);
 	}
 
 	/*
-	 * Service Interface and opration.
+	 * Service Interface and operation. Based on the local service example 
+	 * of class android.app.Service.
 	 */
 
 	private NotificationManager mNM;
-    // Unique Identification Number for the Notification.
-    // We use it on Notification start, and to cancel it.
-    private int NOTIFICATION = R.string.music_player_started;
+	// Unique Identification Number for the Notification.
+	// We use it on Notification start, and to cancel it.
+	private int NOTIFICATION = R.string.music_player_started;
 
-    /* A instance of this class is returned when bind
-     */
-    public class LocalBinder extends Binder {
-        public MusicPlayer musicPlayer() {
-            return MusicPlayer.this;
-        }
-    }
+	/*
+	 * A instance of this class is returned when bind
+	 */
+	public class LocalBinder extends Binder {
+		public MusicPlayer getMusicPlayer() {
+			return MusicPlayer.this;
+		}
+	}
 
-    @Override
-    public void onCreate() {
-        mNM = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+	@Override
+	public void onCreate() {
+		mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
-        // Display a notification about us starting.  We put an icon in the status bar.
-        showNotification();
-    }
+		// Display a notification about us starting. We put an icon in the
+		// status bar.
+		showNotification();
+	}
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("MusicPlayer", "Received start id " + startId + ": " + intent);
-        // We want this service to continue running until it is explicitly
-        // stopped, so return sticky.
-        return START_STICKY;
-    }
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		Log.i("MusicPlayer", "Received start id " + startId + ": " + intent);
+		// We want this service to continue running until it is explicitly
+		// stopped, so return sticky.
+		return START_STICKY;
+	}
 
-    @Override
-    public void onDestroy() {
-        // Cancel the persistent notification.
-        mNM.cancel(NOTIFICATION);
+	@Override
+	public void onDestroy() {
+		// Cancel the persistent notification.
+		mNM.cancel(NOTIFICATION);
 
-        // Tell the user we stopped.
-        Toast.makeText(this, R.string.music_player_stopped, Toast.LENGTH_SHORT).show();
-    }
+		// Tell the user we stopped.
+		Toast.makeText(this, R.string.music_player_stopped, Toast.LENGTH_SHORT)
+				.show();
+	}
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
-    }
+	@Override
+	public IBinder onBind(Intent intent) {
+		return mBinder;
+	}
 
-    // This is the object that receives interactions from clients.  See
-    // RemoteService for a more complete example.
-    private final IBinder mBinder = new LocalBinder();
+	// This is the object that receives interactions from clients. See
+	// RemoteService for a more complete example.
+	private final IBinder mBinder = new LocalBinder();
 
-    /**
-     * Show a notification while this service is running.
-     */
-    private void showNotification() {
-        // Set the icon, scrolling text and timestamp
-        Notification notification = new Notification.Builder(this)
-        	.setContentTitle(getText(R.string.music_player_started))
-        	.setSmallIcon(android.R.drawable.ic_media_play)
-        	.getNotification(); 
-        
-        // The PendingIntent to launch our activity if the user selects this notification
-        //PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-        //        new Intent(this, LocalServiceActivities.Controller.class), 0);
+	/**
+	 * Show a notification while this service is running.
+	 */
+	private void showNotification() {
+		// Set the icon, scrolling text and timestamp
+		Notification notification = new Notification.Builder(this)
+				.setContentTitle(getText(R.string.music_player_started))
+				.setSmallIcon(android.R.drawable.ic_media_play)
+				.getNotification();
 
-        mNM.notify(NOTIFICATION, notification);
-    }
+		// The PendingIntent to launch our activity if the user selects this
+		// notification
+		// PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+		// new Intent(this, LocalServiceActivities.Controller.class), 0);
+
+		mNM.notify(NOTIFICATION, notification);
+	}
 }
--- android/tests/src/jhunovis/musicplayer/test/MusicPlayerTest.java
+++ android/tests/src/jhunovis/musicplayer/test/MusicPlayerTest.java
@@ -1,16 +1,11 @@
 package jhunovis.musicplayer.test;
 
 import jhunovis.musicplayer.MusicPlayer;
-import android.test.AndroidTestCase;
+import android.test.ServiceTestCase;
 import android.util.Log;
 
-import android.net.Uri;
-import android.os.IBinder;
-import android.content.ComponentName;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.res.Resources;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -18,91 +13,88 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import java.util.*;
 import java.io.IOException;
 
+public class MusicPlayerTest extends ServiceTestCase<MusicPlayer> {
 
-public class MusicPlayerTest extends AndroidTestCase {
+	public MusicPlayerTest() {
+		super(MusicPlayer.class);
+	}
 
-		private MusicPlayer mMusicPlayer;
-		
-		/* Represents a connection to our local, private Music Player service.
-		 * 
-		 * This is basically taken from the API docs of android.app.Service.
-		 */
-		private ServiceConnection mConnection = new ServiceConnection() {
-			public void onServiceConnected(ComponentName name, IBinder service) {
-				mMusicPlayer = ((MusicPlayer.LocalBinder)service).musicPlayer();				
-			}
+	private MusicPlayer mMusicPlayer;
 
-			public void onServiceDisconnected(ComponentName name) {
-				mMusicPlayer = null;				
-			}
-		};
-		
-		public MusicPlayerTest() {
-			super();
-		}
-		
-		@Override
-		public void setUp() throws Exception {
-			super.setUp();
-			Intent service = new Intent(getContext(), MusicPlayer.class);
-			getContext().startService(service);
-			getContext().bindService(new Intent(getContext(), 
-		            MusicPlayer.class), mConnection, Context.BIND_AUTO_CREATE);			
-		}
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		startMusicPlayer();
+	}
 
-		@Override
-		public void tearDown() throws Exception {
-			super.tearDown();
-			getContext().unbindService(mConnection);
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		// No shutdown required! super-class already does it.
+		// Double shutdown will result in a null-pointer-exception.
+		// shutdownMusicPlayer();
+	}
+
+	private void startMusicPlayer() {
+		Intent service = new Intent(getContext(), MusicPlayer.class);
+		startService(service);
+		mMusicPlayer = ((MusicPlayer.LocalBinder) bindService(service))
+				.getMusicPlayer();
+	}
+
+	private void shutdownMusicPlayer() {
+		// XXX: this will cause a null pointer exception
+		shutdownService();
+	}
+
+	public void testSetPlaylist() {
+		List<String> playlist = new ArrayList<String>();
+		mMusicPlayer.setPlaylist(playlist);
+		assertEquals(playlist, mMusicPlayer.getPlaylist());
+	}
+
+	public void testPlaybackWithEmptyPlaylist() {
+		mMusicPlayer.setPlaylist(null);
+		try {
+			mMusicPlayer.play();
+		} catch (IOException x) {
+			fail("IOException thrown by player.");
 		}
-		
-		public void testSetPlaylist() {
-			List<String> playlist = new ArrayList<String>(); 
-			mMusicPlayer.setPlaylist(playlist);
-			assertEquals(playlist, mMusicPlayer.getPlaylist());
-		}
-		
-		public void testPlaybackWithEmptyPlaylist() {
-			mMusicPlayer.setPlaylist(null);
-			try {
-				mMusicPlayer.play();
-			}catch(IOException x) {
-				fail("IOException thrown by player.");
-			}
-			assertEquals(false, mMusicPlayer.isPlaying());
-		}
-		
-		public void testPlayback() {
-			String firstTrack = resIdToUri(R.raw.sterzer_for_you);
-			List<String> playlist = Arrays.asList(
-				firstTrack,
+		assertEquals(false, mMusicPlayer.isPlaying());
+	}
+
+	public void testPlayback() {
+		String firstTrack = resIdToUri(R.raw.sterzer_for_you);
+		List<String> playlist = Arrays.asList(firstTrack,
 				resIdToUri(R.raw.tymphony_thrilled));
-			mMusicPlayer.setPlaylist(playlist);
-			try {
-				mMusicPlayer.play();
-			}catch(IOException x) {
-				Log.d("MyLog", Log.getStackTraceString(x));
-				fail("IOException thrown by player.");
-			}
-			assertEquals(true, mMusicPlayer.isPlaying());
-			// this might accidentally fail under race-conditions:
-			assertEquals(firstTrack, mMusicPlayer.currentTrack());
+		mMusicPlayer.setPlaylist(playlist);
+		try {
+			mMusicPlayer.play();
+		} catch (IOException x) {
+			Log.d("MyLog", Log.getStackTraceString(x));
+			fail("IOException thrown by player.");
 		}
-		
-		private String resIdToUri(int resid) {
-			// Access resources of the unit test package instead of the main application.
-			PackageManager pm = getContext().getPackageManager();
-			Resources resources = null;
-			try {
-				resources = pm.getResourcesForApplication(this.getClass().getPackage().getName());
-			} catch (NameNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			// Create a valid Android resource URI for the gived resource id.
-			return ContentResolver.SCHEME_ANDROID_RESOURCE + "://" 
-				+ resources.getResourcePackageName(resid) + '/' 
-				+ resources.getResourceTypeName(resid) + '/' 
-				+ resources.getResourceEntryName(resid);			
+		assertEquals(true, mMusicPlayer.isPlaying());
+		// this might accidentally fail under race-conditions:
+		assertEquals(firstTrack, mMusicPlayer.currentTrack());
+	}
+
+	private String resIdToUri(int resid) {
+		// Access resources of the unit test package instead of the main
+		// application.
+		PackageManager pm = getContext().getPackageManager();
+		Resources resources = null;
+		try {
+			resources = pm.getResourcesForApplication(this.getClass()
+					.getPackage().getName());
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		// Create a valid Android resource URI for the gived resource id.
+		return ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+				+ resources.getResourcePackageName(resid) + '/'
+				+ resources.getResourceTypeName(resid) + '/'
+				+ resources.getResourceEntryName(resid);
+	}
 }
